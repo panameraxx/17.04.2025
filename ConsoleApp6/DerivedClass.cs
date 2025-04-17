d847718ca6e3d96850a9dbca5ece9036028f107c
panameraxx <90213411+panameraxx@users.noreply.github.com>
2025-04-17 11:05:07 +0300

    Add files via upload

---
+++ ConsoleApp6/DerivedClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class DerivedClass:IInterface3
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Метод Method1");
+        }
+        public void Method2()
+        {
+            Console.WriteLine("Метод Method2");
+        }
+        public void Method3()
+        {
+            Console.WriteLine("Метод Method3");
+        }
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass dClass = new DerivedClass();
+            dClass.Method1();
+            dClass.Method2();
+            dClass.Method3();
+            Console.ReadLine();
+        }
+    }
+}
