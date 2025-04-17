4fa64250da7afb17c1831b1a522c7b60d2958044
panameraxx <90213411+panameraxx@users.noreply.github.com>
2025-04-17 10:54:41 +0300

    Add files via upload

---
+++ ConsoleApp5/IMyInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+   interface IMyInterface
+    {
+        double MyGetPi();
+        int MyGetInt();
+        int MySquare(int x);
+        double MySqrt(double x);
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            Console.WriteLine($"метод, возвращающий число типа int = {myClass.MyGetInt()}");
+            Console.WriteLine($"метод, возвращающий число PI = {myClass.MyGetPi()}");
+            Console.WriteLine($"метод, возвращающий x в квадрате = {myClass.MySquare(3)}");
+            Console.WriteLine($"метод, возвращающий корень квадратный из x = {myClass.MySqrt(6.25)}");
+            Console.ReadKey();
+        }
+    }
+}
+
